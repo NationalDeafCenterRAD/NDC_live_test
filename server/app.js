5e20ce5638edb33ab3d036a7e681e61088a5a272
Jonah Winninghoff <jonahwinninghoff@Jonahs-iMac.local>
2024-10-29 16:45:20 -0700

    Test cloudways once again.

--- server/app.js
+++ server/app.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files from the React build
-app.use(express.static(path.join(__dirname, '../public_html')));
+app.use(express.static(path.join(__dirname, '/')));
 
 // For any other route, serve the React app
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public_html', 'index.html'));
+  res.sendFile(path.join(__dirname, '/', 'index.html'));
 });
 
 app.listen(process.env.PORT || 3000, () => {
