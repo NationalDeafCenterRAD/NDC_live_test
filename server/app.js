fee17a2da642638fd27f5987897f26c01011bceb
Jonah Winninghoff <jonahwinninghoff@Jonahs-iMac.local>
2024-10-29 15:46:33 -0700

    Change this structure for cloudways.

---
+++ server/app.js
@@ -0,0 +1,16 @@
+// App
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve static files from the React build
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+// For any other route, serve the React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Server is running');
+})
