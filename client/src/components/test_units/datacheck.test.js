682c1bbf6442da7ec1a06d535e2c6bc4288ef2b2
NationalDeafCenterRAD <rad@nationaldeafcenter.org>
2025-12-14 16:46:28 -0800

    Revised one test unit.

--- client/src/components/test_units/datacheck.test.js
+++ client/src/components/test_units/datacheck.test.js
@@ -273,11 +273,11 @@ describe('Data Integrity Checks', () => {
       allVariables.forEach(variable => {
         allStates.forEach(state => {
           const emp = employmentData.filter(item => item.variable === variable && item.status === status && item.state === state).map(item => item.percentage == null ? item.median_income : item.percentage);
-          const areAllZeroesOr100s = status === 'no HS diploma' ?  
-            emp.length === 0  || emp.every(value => value === 0) != true : 
-            emp.length === 0 || emp.every(value => value === 0 || value === 100 || value === emp[0]) != true;
+          const isOk = status === 'no HS diploma'
+            ? !(emp.length > 0 && emp.every(value => value === 0))
+            : !(emp.length > 0 && emp.every(value => value === 0 || value === 100));
           try{
-            expect(areAllZeroesOr100s).toBe(true);
+            expect(isOk).toBe(true);
           } catch(error){
             const explanation = `Found missing info in data. In the state "${state}", status "${status}" and variable "${variable}", this data returns array of percentages: [${emp}]. Please review your R script work. If good, revise the datacheck.test.js.`
             throw new Error(explanation);
